14cc05078b295aac4f983765b9b1498a5adf2b5e
lindsim <lindsey@localhost.(none)>
2015-09-05 21:38:49 -0700

    finished markovgen for making fortunes

--- markovgen.js
+++ markovgen.js
@@ -1,50 +1,63 @@
 //get the text file
 fs = require('fs')
 fs.readFile('lyrics.txt', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
+	if (err) {
+    	return console.log(err);
+  	}
 //console.log("file accepted!");
 
-//removes anything that isn't alpha or single apostrophe
-//removes multiple adjacent spaces into single spaces
+//replaces anything that isn't alpha or single apostrophe with single space
+//replaces multiple adjacent spaces into single spaces with single space
+//sends this to makeDict function then makeChain function then the makeFortune function
+	else {
+		
+		var dictionary = makeDict(data.replace(/[^a-zA-Z']/g," ").replace(/\s+/g, " ").toLowerCase().split(" "));
+		for (var x = 0; x < 100; x++){
+			console.log(makeFortune(dictionary)+"." + "   ");
+		}
+	}
 
-var editedData = data.replace(/[^a-zA-Z']/g," ").replace(/\s+/g, " ");
-
-
-//console.log(editedData);
 });
 
 
+//make dictionary of word to words that follow that word, including dupes
 
 
-// for (var i = 0; i < titles.length; i++) {
-//     var words = titles[i].split(' ');
-//     terminals[words[words.length-1]] = true;
-//     startwords.push(words[0]);
-//     for (var j = 0; j < words.length - 1; j++) {
-//         if (wordstats.hasOwnProperty(words[j])) {
-//             wordstats[words[j]].push(words[j+1]);
-//         } else {
-//             wordstats[words[j]] = [words[j+1]];
-//         }
-//     }
-// }
+var makeDict = function(text){
+	var dict = {};
+	for (var n = 0; n < text.length-1; n++){
+		if (!dict[text[n]]){
+			dict[text[n]] = [text[n+1]];
+		}
+		else {
+			dict[text[n]].push(text[n+1]);
+		}
+	}
+	return dict;
 
-// var choice = function (a) {
-//     var i = Math.floor(a.length * Math.random());
-//     return a[i];
-// };
+}
 
-// var make_fortune = function (min_length) {
-//     word = choice(startwords);
-//     var fortune = [word];
-//     while (wordstats.hasOwnProperty(word)) {
-//         var next_words = wordstats[word];
-//         word = choice(next_words);
-//         fortune.push(word);
-//         if (fortune.length > min_length && terminals.hasOwnProperty(word)) break;
-//     }
-//     if (fortune.length < min_length) return make_fortune(min_length);
-//     return fortune.join(' ');
-// };
+//create the fortune and return it
+var makeFortune = function(dict, num){
+	var keys = Object.keys(dict);
+	var first = choose(keys);
+	var current = choose(dict[first]);
+	var fortune = first;
+	var minLength = 60;
+
+	while(fortune.length < minLength){
+	  	fortune = fortune + " " + current;
+	  	current = choose(dict[current]);	
+	 }
+
+	return fortune;
+
+}
+	
+
+//helper function for choosing random value from key
+var choose = function(words){
+	var i = Math.floor(Math.random() * (words.length - 0)) + 0;
+	return words[i];
+}
+
