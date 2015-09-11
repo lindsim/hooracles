3891b8226f0fbf264cb920e39704493f91d79fc0
lindsim <lindsey@localhost.(none)>
2015-09-10 17:25:39 -0700

    completed app

--- public/fortunes.js
+++ public/fortunes.js
@@ -1,3 +1,4 @@
+$('#1').hide();
 
 $('button').click(function(){
 	$.get('/fortunes', appendToH);
@@ -5,6 +6,9 @@ $('button').click(function(){
 		var i = Math.floor(Math.random() * (fortunes.length - 0)) + 0;
 		$('body').append('<p>'+ fortunes[i]+ '</p>');
 		$('button').hide();
+		$('#0').hide();
+		$('#1').show();
+
 		
 	}
 });
@@ -12,6 +16,9 @@ $('button').click(function(){
 $('body').click(function(){
 	$('p').hide();
 	$('button').show();
+	$('#1').hide();
+	$('#0').show();
+
 	
 })
 
